74a769de0fae8674f8eea04db75be32be301636f
OkaniYoshiii <dragut31620@gmail.com>
2026-03-21 18:03:02 +0100

    feat: ajout fonctions pour modification niveau et expérience

--- src/character.ts
+++ src/character.ts
@@ -57,6 +57,29 @@ function validateAbilityModifier(value: number) {
     );
 }
 
+function validateExperience(value: number) {
+    const min = 0;
+    const max = 355000;
+    if (value < 0 || value > max) {
+        return err(
+            new Error(
+                `Experience value must be greater than ${min} and less than ${max}`,
+            ),
+        );
+    }
+
+    return ok(value);
+}
+
+function validateLevel(value: number) {
+    const min = 0;
+    if (value < 0) {
+        return err(new Error(`Level value must be greater than ${min}`));
+    }
+
+    return ok(value);
+}
+
 export function defaultAbilities(): AbilitiesStats {
     return {
         [Ability.Strength]: 10,
@@ -245,6 +268,112 @@ export function setAbilityModifier(ability: Ability, modifier: number) {
 
     const element = result.value;
     element.textContent = modifier.toString();
+
+    return ok();
+}
+
+export function getExperienceInput() {
+    const id = "character_experience";
+    const element_result = getElementById(id);
+    if (element_result.isErr()) {
+        return element_result;
+    }
+
+    const experience_input = element_result.value;
+    if (!(experience_input instanceof HTMLInputElement)) {
+        return err(
+            new Error(
+                `Expected element with id ${id} to be an instance of ${HTMLInputElement.name}`,
+            ),
+        );
+    }
+
+    return ok(experience_input);
+}
+
+export function setExperience(experience: number) {
+    const validation_result = validateExperience(experience);
+    if (validation_result.isErr()) {
+        return validation_result;
+    }
+
+    const experience_input_result = getExperienceInput();
+    if (experience_input_result.isErr()) {
+        return experience_input_result;
+    }
+
+    const experience_input = experience_input_result.value;
+    experience_input.value = experience.toString();
+
+    let level = 1;
+    if (experience < 300) {
+        level = 1;
+    } else if (experience >= 300) {
+        level = 2;
+    } else if (experience >= 900) {
+        level = 3;
+    } else if (experience >= 2700) {
+        level = 4;
+    } else if (experience >= 6500) {
+        level = 5;
+    } else if (experience >= 14000) {
+        level = 6;
+    } else if (experience >= 23000) {
+        level = 7;
+    } else if (experience >= 34000) {
+        level = 8;
+    } else if (experience >= 48000) {
+        level = 9;
+    } else if (experience >= 64000) {
+        level = 10;
+    } else if (experience >= 85000) {
+        level = 11;
+    } else if (experience >= 100000) {
+        level = 12;
+    } else if (experience >= 120000) {
+        level = 13;
+    } else if (experience >= 140000) {
+        level = 14;
+    } else if (experience >= 165000) {
+        level = 15;
+    } else if (experience >= 195000) {
+        level = 16;
+    } else if (experience >= 225000) {
+        level = 17;
+    } else if (experience >= 265000) {
+        level = 18;
+    } else if (experience >= 305000) {
+        level = 19;
+    } else if (experience >= 355000) {
+        level = 20;
+    }
+
+    return setLevel(level);
+}
+
+export function getLevelElement() {
+    const id = "character_level";
+    const element_result = getElementById(id);
+    if (element_result.isErr()) {
+        return element_result;
+    }
+
+    return ok(element_result.value);
+}
+
+export function setLevel(level: number) {
+    const validation_result = validateLevel(level);
+    if (validation_result.isErr()) {
+        return validation_result;
+    }
+
+    const level_element_result = getLevelElement();
+    if (level_element_result.isErr()) {
+        return level_element_result;
+    }
+
+    const level_element = level_element_result.value;
+    level_element.textContent = level.toString();
 
     return ok();
 }
