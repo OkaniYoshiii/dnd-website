9cde339f1a84c4ae1f1722956787f6216e3c2c15
OkaniYoshiii <dragut31620@gmail.com>
2026-03-22 01:32:58 +0100

    feat: gestion de la modification de l'expérience

--- src/character.ts
+++ src/character.ts
@@ -344,46 +344,66 @@ export function setExperience(experience: number): Result<void, Error> {
             input.value = experience.toString();
 
             let level = 1;
-            if (experience < 300) {
-                level = 1;
-            } else if (experience >= 300) {
-                level = 2;
-            } else if (experience >= 900) {
-                level = 3;
-            } else if (experience >= 2700) {
-                level = 4;
-            } else if (experience >= 6500) {
-                level = 5;
-            } else if (experience >= 14000) {
-                level = 6;
-            } else if (experience >= 23000) {
-                level = 7;
-            } else if (experience >= 34000) {
-                level = 8;
-            } else if (experience >= 48000) {
-                level = 9;
-            } else if (experience >= 64000) {
-                level = 10;
-            } else if (experience >= 85000) {
-                level = 11;
-            } else if (experience >= 100000) {
-                level = 12;
-            } else if (experience >= 120000) {
-                level = 13;
-            } else if (experience >= 140000) {
-                level = 14;
-            } else if (experience >= 165000) {
-                level = 15;
-            } else if (experience >= 195000) {
-                level = 16;
-            } else if (experience >= 225000) {
-                level = 17;
-            } else if (experience >= 265000) {
-                level = 18;
-            } else if (experience >= 305000) {
-                level = 19;
-            } else if (experience >= 355000) {
-                level = 20;
+            switch (true) {
+                case experience < 300:
+                    level = 1;
+                    break;
+                case experience < 900:
+                    level = 2;
+                    break;
+                case experience < 2700:
+                    level = 3;
+                    break;
+                case experience < 6500:
+                    level = 4;
+                    break;
+                case experience < 14000:
+                    level = 5;
+                    break;
+                case experience < 23000:
+                    level = 6;
+                    break;
+                case experience < 34000:
+                    level = 7;
+                    break;
+                case experience < 48000:
+                    level = 8;
+                    break;
+                case experience < 64000:
+                    level = 9;
+                    break;
+                case experience < 85000:
+                    level = 10;
+                    break;
+                case experience < 100000:
+                    level = 11;
+                    break;
+                case experience < 120000:
+                    level = 12;
+                    break;
+                case experience < 140000:
+                    level = 13;
+                    break;
+                case experience < 165000:
+                    level = 14;
+                    break;
+                case experience < 195000:
+                    level = 15;
+                    break;
+                case experience < 225000:
+                    level = 16;
+                    break;
+                case experience < 265000:
+                    level = 17;
+                    break;
+                case experience < 305000:
+                    level = 18;
+                    break;
+                case experience < 355000:
+                    level = 19;
+                    break;
+                default:
+                    level = 20;
             }
 
             return setLevel(level);
--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,26 @@
-import { getClassSelect } from "./character";
+import { err } from "neverthrow";
+import { getExperienceInput, setExperience } from "./character";
 
 function handleError(error: Error) {
     console.error(error);
 }
 
 (function (): void {
-    const result = getClassSelect();
+    getExperienceInput().match((input) => {
+        input.addEventListener("change", updateExperience);
+    }, handleError);
+})();
+
+function updateExperience() {
+    const result = getExperienceInput().andThen((input) => {
+        const experience = parseInt(input.value);
+        if (isNaN(experience)) {
+            return err(new Error("Experience is NaN"));
+        }
+        return setExperience(experience);
+    });
+
     if (result.isErr()) {
         return handleError(result.error);
     }
-
-    const class_select = result.value;
-
-    class_select.addEventListener("change", updateCharacterClass);
-})();
-
-function updateCharacterClass(): void {}
+}
